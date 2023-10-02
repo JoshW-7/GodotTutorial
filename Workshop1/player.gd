59eb69839041a764b792ba8f91f1b3c2a69bd165
Josh <joshwiens7@gmail.com>
2023-10-02 10:17:58 -0700

    add sfx to workshop1, hit animation to workshop 0 and 1 (not used in workshop 0)

--- Workshop1/player.gd
+++ Workshop1/player.gd
@@ -9,6 +9,11 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var start_position
 
+enum State {
+	NORMAL,
+	HIT
+}
+var state := State.NORMAL
 
 func _ready():
 	start_position = global_position
@@ -21,6 +26,7 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$Jump.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -32,16 +38,25 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
-	if velocity.x != 0:
-		$AnimationPlayer.play("walk")
-	else:
-		$AnimationPlayer.play("idle")
-		
-	if Input.is_action_just_pressed("ui_left"):
-		$Sprite2D.flip_h = true
-	elif Input.is_action_just_pressed("ui_right"):
-		$Sprite2D.flip_h = false
+	match state:
+		State.NORMAL:
+			if velocity.x != 0:
+				$AnimationPlayer.play("walk")
+			else:
+				$AnimationPlayer.play("idle")
+				
+			if Input.is_action_just_pressed("ui_left"):
+				$Sprite2D.flip_h = true
+			elif Input.is_action_just_pressed("ui_right"):
+				$Sprite2D.flip_h = false
+		State.HIT:
+			pass
 
 
 func _on_area_2d_body_entered(body):
+	state = State.HIT
+	$Hit.play()
+	$AnimationPlayer.play("hit")
+	await $AnimationPlayer.animation_finished
 	global_position = start_position
+	state = State.NORMAL
