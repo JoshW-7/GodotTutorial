2db7ed18604152ab9772237a23e3c3b967a7bc50
Josh <joshwiens7@gmail.com>
2023-10-02 11:40:49 -0700

    update

--- Workshop1/player.gd
+++ Workshop1/player.gd
@@ -22,39 +22,40 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		$Jump.play()
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
 	
 	match state:
 		State.NORMAL:
+			# Handle Jump.
+			if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+				velocity.y = JUMP_VELOCITY
+				$Jump.play()
+
+			# Get the input direction and handle the movement/deceleration.
+			# As good practice, you should replace UI actions with custom gameplay actions.
+			var direction = Input.get_axis("ui_left", "ui_right")
+			if direction:
+				velocity.x = direction * SPEED
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 			if velocity.x != 0:
 				$AnimationPlayer.play("walk")
 			else:
 				$AnimationPlayer.play("idle")
 				
-			if Input.is_action_just_pressed("ui_left"):
+			if Input.is_action_pressed("ui_left"):
 				$Sprite2D.flip_h = true
-			elif Input.is_action_just_pressed("ui_right"):
+			elif Input.is_action_pressed("ui_right"):
 				$Sprite2D.flip_h = false
 		State.HIT:
 			pass
+			
+	move_and_slide()
 
 
 func _on_area_2d_body_entered(body):
 	state = State.HIT
+	velocity = Vector2.ZERO
 	$Hit.play()
 	$AnimationPlayer.play("hit")
 	await $AnimationPlayer.animation_finished
